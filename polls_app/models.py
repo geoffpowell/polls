2645298d853cebc4ab86e98a5851d1f11016d557
Geoff Powell <geoff@geoffpowell.net>
2015-08-13 17:53:17 -0700

    changed __str__ to __unicode__

--- polls_app/models.py
+++ polls_app/models.py
@@ -5,9 +5,13 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def __unicode__(self): #using unicode because python 2. Python 3 should use __str__
+    		return self.question_text
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __unicode__(self): #using unicode because python 2. Python 3 should use __str__
+    		return self.choice_text
