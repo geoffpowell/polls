d099f6d59c5e899a2ac8fbdfc488694747e5a9ed
Geoff Powell <geoff@geoffpowell.net>
2015-08-17 22:27:05 -0700

    notes on reordering admin form fields.

--- polls_app/admin.py
+++ polls_app/admin.py
@@ -5,6 +5,7 @@ from .models import Question
 # Register your models here.
 # admin.site.register(Question)
 
+#This basically does the same thing as the registration above but this reorders the pub_date and question_text. Comes in handy when there are many different admin forms where the order needs to be intuitive.
 class QuestionAdmin(admin.ModelAdmin):
 		fields = ['pub_date', 'question_text']
 
