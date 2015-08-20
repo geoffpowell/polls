1ead77294d3c20b726036ec94cf64ae13b216352
Geoff Powell <geoff@geoffpowell.net>
2015-08-19 19:07:49 -0700

    made improvements to the question model

--- polls_app/models.py
+++ polls_app/models.py
@@ -11,7 +11,10 @@ class Question(models.Model):
     def __unicode__(self): #using __unicode__ method because python 2. Python 3 should use __str__.
     		return self.question_text
     def was_published_recently(self):
-    		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
