e48a36e67de3e738c546521a1983b8e47f9b33bb
Geoff Powell <geoff@geoffpowell.net>
2015-09-02 14:40:50 -0700

    replaced the index.html view with a nice django shortcut

--- polls_app/views.py
+++ polls_app/views.py
@@ -1,18 +1,15 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.template import RequestContext, loader
 
 from .models import Question
 # Create your views here.
 # def index(request):
 # 		return HttpResponse("Hello! I'm your homepage!")
 def index(request):
-		latest_question_list = Question.objects.order_by('-pub_date')[:5]
-		template = loader.get_template('polls_app/index.html')
-		context = RequestContext(request, {
-				'latest_question_list': latest_question_list,
-			})
-		return HttpResponse(template.render(context))
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls_app/index.html', context)
 		# output = ', '.join([p.question_text for p in latest_question_list])
 		# return HttpResponse(output)
 
