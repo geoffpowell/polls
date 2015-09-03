ca9cc0a95ca8af73cce1140e8fda356119d78ff4
Geoff Powell <geoff@geoffpowell.net>
2015-09-02 17:02:24 -0700

    updated the detail view using a django shortcut import instead

--- polls_app/views.py
+++ polls_app/views.py
@@ -1,5 +1,5 @@
-from django.http import Http404
-from django.shortcuts import render
+# from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 # from django.template import RequestContext, loader
 
@@ -25,8 +25,5 @@ def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
 def detail(request, question_id):
-		try:
-				question = Question.objects.get(pk=question_id)
-		except Question.DoesNotExist:
-				raise Http404("Question does not exist.")
+		question = get_object_or_404(Question, pk=question_id)
 		return render(request, 'polls_app/detail.html', {'question': question})
