0773f191cb8ac2b54488971756455f5bb842b2a3
Geoff Powell <geoff@geoffpowell.net>
2015-09-07 16:58:39 -0700

    added a test to test whether a boolean for 30 days in the future is false

--- polls_app/tests.py
+++ polls_app/tests.py
@@ -1,3 +1,23 @@
+
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
+from .models import Question
+
 # Create your tests here.
+
+
+
+class QuestionMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is in the future.
+        """
+        #specifically tests a boolean for a date 30 days in the future
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
